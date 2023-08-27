b4fb211aa18485676996a7620efdd10916fb4b4d
Vikram Kumar Bathala <vbathala@asu.edu>
2023-08-27 00:52:35 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
-variable "accesskey"{
-default="AKIA5M6JNI3LRR5JYZA5"
+variable "access_key" {
+  description = "AWS Access Key"
+  default     = "AKIA5M6JNI3LRR5JYZA5"
 }
 
-variable "secretkey"{
-default="Lvn+ecR0OvT2pCGEShVF583bY7+7nvFf+Ehiu/e4"
+variable "secret_key" {
+  description = "AWS Secret Key"
+  default     = "Lvn+ecR0OvT2pCGEShVF583bY7+7nvFf+Ehiu/e4"
 }
