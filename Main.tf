cf9e1efee96d48084fd0e5b5d958bc9f37dcc359
Developer1 <vbathala@asu.edu>
2023-09-30 17:58:37 -0500

    Changing the code structure

---
+++ Main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region="us-east-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
+    
+}
+
+
+resource "aws_security_group" "rtp03-sg" {
+    name = "rtp03-sg"
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ssh-sg"
+
+    }
+}
